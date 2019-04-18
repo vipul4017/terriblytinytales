adaf170655be39300c0e8e5945d8a10f216db4ee
vipul garg <vipul@so.city>
2019-04-18 18:39:19 +0530

    dirname fixes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(___dirname + '/public/favicon.ico'))
+app.use(favicon(__dirname + '/public/favicon.ico'))
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -41,7 +41,7 @@ app.options('*', (req, res, next) => {
 
 
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/words', wordsRouter);
 
 //catch errors
